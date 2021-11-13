9b8dad2219f63281f7dea7f83131c7312d0c10e3
Komal Nida <komalnids452@gmail.com>
2021-11-13 17:49:17 +0500

    New

--- app.js
+++ app.js
@@ -46,4 +46,6 @@ app.get("/", (req, res) => {
   res.send("Home Page ...");
 });
 
-app.listen(env.process.PORT, () => console.log("SERVER RUNNING on 8080"));
+app.listen(env.process.PORT || 8080, () =>
+  console.log(`SERVER RUNNING on ${port}`)
+);
